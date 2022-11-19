45b0f7b37e5d789c96fa343767c9d898701aeaee
Isabel Bonnet <109360978+ibonnet@users.noreply.github.com>
2022-11-18 21:23:03 -0500

    Update venezuelanhospitals-oct22.js

--- Encuesta Nacional de Hospitales/2022/venezuelanhospitals-oct22.js
+++ Encuesta Nacional de Hospitales/2022/venezuelanhospitals-oct22.js
@@ -19,7 +19,7 @@ d3.csv("venezuelanhospitals-oct22.csv", function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, height])
+    .domain([0, width])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
