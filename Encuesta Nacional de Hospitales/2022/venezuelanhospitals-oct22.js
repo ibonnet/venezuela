a6dff2bd2c849ef6966f1860723c97834b82d6a3
Isabel Bonnet <109360978+ibonnet@users.noreply.github.com>
2022-11-29 12:04:28 -0500

    Update venezuelanhospitals-oct22.js

--- Encuesta Nacional de Hospitales/2022/venezuelanhospitals-oct22.js
+++ Encuesta Nacional de Hospitales/2022/venezuelanhospitals-oct22.js
@@ -3,7 +3,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 20, bottom: 30, left: 50},
     width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 420 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#venezuelanhospitals-oct22")
