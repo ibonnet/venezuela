1742a55532120b3b7da7104f81de182794747066
Isabel Bonnet <109360978+ibonnet@users.noreply.github.com>
2023-02-08 00:18:09 -0500

    Update qs-rankings.js
    New intervals and bugs fixed with the tooltip

--- universidades/qs-rankings/qs-rankings.js
+++ universidades/qs-rankings/qs-rankings.js
@@ -1,7 +1,7 @@
 
   // set the dimensions and margins of the graph
-  var margin = {top: 10, right: 90, bottom: 50, left: 80},
-      width = 650 - margin.left - margin.right,
+  var margin = {top: 30, right: 150, bottom: 50, left: 80},
+      width = 750 - margin.left - margin.right,
       height = 450 - margin.top - margin.bottom;
   
   // append the svg object to the body of the page
@@ -12,7 +12,38 @@
     .append("g")
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
-  
+
+// Legend
+// UCV
+graph.append("circle").attr("cx",528).attr("cy",25).attr("r", 5).style("fill", "#3B3C96")
+graph.append("text").attr("x", 534).attr("y", 25).text("Universidad Central de Venezuela").style("font-size", "9px").attr("alignment-baseline","middle")
+// ULA
+graph.append("circle").attr("cx",528).attr("cy",40).attr("r", 5).style("fill", "#A2206B")
+graph.append("text").attr("x", 534).attr("y", 40).text("Universidad de Los Andes").style("font-size", "9px").attr("alignment-baseline","middle")
+
+// Title
+graph.append("text").attr("x", 220).attr("y", 408).text("Year").style("font-size", "13px").attr("alignment-baseline","middle").style("font-family", "georgia")
+graph.append("text").attr("x", 20).attr("y", -15).text("World Rankings of Venezuelan Universities since 2013").style("font-size", "16px").attr("alignment-baseline","middle").style("font-family", "georgia").style("font-weight","bold")
+graph.append("text").attr("x", -250).attr("y", -65).text("World University Rankings").style("font-size", "13px").attr("transform", "rotate(-90)").style("font-family", "georgia")
+
+// Y Axis
+graph.append("text").attr("x", -50).attr("y", 21).text("551-600").style("font-size", "10px").attr("alignment-baseline","middle")
+graph.append("text").attr("x", -50).attr("y", 63).text("601-650").style("font-size", "10px").attr("alignment-baseline","middle")
+graph.append("text").attr("x", -50).attr("y", 104).text("651-700").style("font-size", "10px").attr("alignment-baseline","middle")
+graph.append("text").attr("x", -50).attr("y", 145).text("701-750").style("font-size", "10px").attr("alignment-baseline","middle")
+graph.append("text").attr("x", -52).attr("y", 186).text("751-800").style("font-size", "10px").attr("alignment-baseline","middle")
+graph.append("text").attr("x", -55).attr("y", 290).text("801-1000").style("font-size", "10px").attr("alignment-baseline","middle")
+
+graph.append("text").attr("x", 330).attr("y", 415).text("Source: QS World Rankings / Graph: Isabel Bonnet").style("font-size", "8px").attr("alignment-baseline","middle")
+
+// intervals rectangles
+graph.append("rect").attr("x",0).attr("y",0).attr("width",520).attr("height",41).style("fill", "#66A54C").style("opacity", ".13")
+graph.append("rect").attr("x",0).attr("y",41).attr("width",520).attr("height",41).style("fill", "#257070").style("opacity", ".13")
+graph.append("rect").attr("x",0).attr("y",82).attr("width",520).attr("height",41).style("fill", "#773C66").style("opacity", ".13")
+graph.append("rect").attr("x",0).attr("y",123).attr("width",520).attr("height",41).style("fill", "#3B3C96").style("opacity", ".13")
+graph.append("rect").attr("x",0).attr("y",164).attr("width",520).attr("height",41).style("fill", "#391833C").style("opacity", ".13")
+graph.append("rect").attr("x",0).attr("y",205).attr("width",520).attr("height",165).style("fill", "#91483C").style("opacity", ".13")
+
   //Read the data
   d3.csv("qs-rankings.csv", function(data) {
   
@@ -25,20 +56,19 @@
     var x = d3.scaleLinear()
       .domain([2013,2023])
       .range([ 0, width ]);
-    graph.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(10));
+
+    const xAxis = d3.axisBottom(x)
+      .tickFormat(d3.format("d"))
   
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([550, 950])
+      .domain([551, 1000])
       .range([ 0, height]);
+   
+   
+    graph.append("g").call(d3.axisLeft(y).ticks(0));
+     graph.append("g").call(xAxis).attr("transform", "translate(0," + height + ")")
 
-    graph.append("g")
-     .call(d3.axisLeft(y)
-     .ticks(0))
-     ;
-  
     // var ynew = d3.axisLeft().scale(y)
     //   .tickValues([475.5, 525.5, 576.5, 625.5, 675.5, 725.5, 775.5, 900.5, 1100.5, 1300.5])
     //   .tickFormat((d,i) => ["471-480", "501-550", "601-650", "651-700", "701-750", "751-800", "801-1000", "1001-1200", "1201-1400"][i]);
@@ -46,7 +76,7 @@
     //   graph.append("g")
     //   .call(d3.axisLeft(ynew).ticks(9));
 
-    // color palette
+// color palette
     var res = sumstat.map(function(d){ return d.key }) // list of group names
     var color = d3.scaleOrdinal()
         .domain(["UCV","ULA" ])
@@ -73,7 +103,7 @@
   }
   var mousemove = function(d) {
     Tooltip
-      .html("Ranking: " + d.rankingnumber + "<br>University: " + d.university)
+      .html("In " + d.date + ", " + d.university + " ranked " + d.rankingnumber + " in the QS World University Rankings.")
       .style("left", (d3.mouse(this)[0]+70) + "10px")
       .style("top", (d3.mouse(this)[1]) + "100px")
   }
@@ -131,29 +161,6 @@
   .on("mouseover", mouseover)
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave)
-
-  // Legend
-// UCV
-graph.append("circle").attr("cx",500).attr("cy",25).attr("r", 5).style("fill", "#3B3C96")
-graph.append("text").attr("x", 510).attr("y", 25).text("UCV").style("font-size", "10px").attr("alignment-baseline","middle")
-// ULA
-graph.append("circle").attr("cx",500).attr("cy",40).attr("r", 5).style("fill", "#A2206B")
-graph.append("text").attr("x", 510).attr("y", 40).text("ULA").style("font-size", "10px").attr("alignment-baseline","middle")
-
-// Title
-graph.append("text").attr("x", 50).attr("y", 0).text("World Rankings of Venezuelan Universities since 2013").style("font-size", "16px").attr("alignment-baseline","middle").style("font-family", "georgia").style("font-weight","bold")
-graph.append("text").attr("x", 230).attr("y", 425).text("Year").style("font-size", "13px").attr("alignment-baseline","middle").style("font-family", "georgia")
-graph.append("text").attr("x", -250).attr("y", -70).text("World University Rankings").style("font-size", "13px").attr("transform", "rotate(-90)").style("font-family", "georgia")
-
-// Y Axis
-graph.append("text").attr("x", -50).attr("y", 25).text("551-600").style("font-size", "10px").attr("alignment-baseline","middle")
-graph.append("text").attr("x", -50).attr("y", 75).text("601-650").style("font-size", "10px").attr("alignment-baseline","middle")
-graph.append("text").attr("x", -50).attr("y", 125).text("651-700").style("font-size", "10px").attr("alignment-baseline","middle")
-graph.append("text").attr("x", -50).attr("y", 175).text("701-750").style("font-size", "10px").attr("alignment-baseline","middle")
-graph.append("text").attr("x", -52).attr("y", 225).text("751-800").style("font-size", "10px").attr("alignment-baseline","middle")
-graph.append("text").attr("x", -55).attr("y", 340).text("801-1000").style("font-size", "10px").attr("alignment-baseline","middle")
-
-graph.append("text").attr("x", 330).attr("y", 435).text("Source: QS World Rankings / Graph: Isabel Bonnet").style("font-size", "8px").attr("alignment-baseline","middle")
 
 })
 
